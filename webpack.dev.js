094bdd95af32a52c0dd0bb691d5761f1c4e5b6b2
luckylooky2 <luckylooky2@naver.com>
2024-08-21 09:52:31 +0900

    Env: 개발 서버에서 eslint를 사용하기 위한 플러그인 추가

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,19 +1,24 @@
-const { merge } = require("webpack-merge");
-const common = require("./webpack.common.js");
-const path = require("path");
+const ESLintPlugin = require('eslint-webpack-plugin');
+const { merge } = require('webpack-merge');
+
+const common = require('./webpack.common.js');
 
 module.exports = merge(common, {
-  mode: "development",
-  devtool: "inline-source-map",
+  mode: 'development',
+  devtool: 'inline-source-map',
   devServer: {
     port: 3000,
     hot: true,
-    open: true,
+    open: false,
     compress: true,
-    static: { directory: path.join(__dirname, "/dist") },
     client: {
-      logging: "info",
+      logging: 'info',
       overlay: true,
     },
   },
+  plugins: [
+    new ESLintPlugin({
+      emitWarning: true,
+    }),
+  ],
 });
