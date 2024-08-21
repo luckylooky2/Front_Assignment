9b4cd74290fc30c84ef14296644a873855d1ccc7
luckylooky2 <luckylooky2@naver.com>
2024-08-21 11:49:09 +0900

    Fix: 개발 eslint 서버 대상 확장자에 jsx 추가

--- webpack.dev.js
+++ webpack.dev.js
@@ -19,6 +19,8 @@ module.exports = merge(common, {
   plugins: [
     new ESLintPlugin({
       emitWarning: true,
+      extensions: ['js', 'jsx'],
+      exclude: 'node_modules',
     }),
   ],
 });
