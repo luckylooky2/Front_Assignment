e8881ad75d36e4593ac8acaa10fdac83d686a280
luckylooky2 <luckylooky2@naver.com>
2024-08-23 11:42:03 +0900

    Fix: DroppableWrapper props 변경 및 handleDragUpdate 추가

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useCallback } from 'react';
+import React, { useState } from 'react';
 import { DragDropContext } from 'react-beautiful-dnd';
 
 import DroppableWrapper from './DroppableWrapper';
@@ -10,41 +10,66 @@ export default function App() {
   const [itemLists, setItemLists] = useState(
     createListsUpToColumnCount(COLUMN_COUNT, 10),
   );
-  const [srcDroppableIdx, setSrcDroppableIdx] = useState(-1);
+  const [srcDraggable, setSrcDraggable] = useState(null);
+  const [dstDraggable, setDstDraggable] = useState(null);
   const style = getLayoutStyles;
 
-  const onDragEnd = useCallback(
-    (result) => {
-      if (!result.destination) {
-        return;
-      }
-      const { droppableId: startId, index: startRow } = result.source;
-      const { droppableId: endId, index: endRow } = result.destination;
-      const [startCol, endCol] = [Number(startId.at(-1)), Number(endId.at(-1))];
-      const newItems = reorder(
-        itemLists,
-        [startCol, startRow],
-        [endCol, endRow],
-      );
-      setItemLists(newItems);
-    },
-    [itemLists],
-  );
+  // console.log(itemLists);
 
-  const onDragStart = useCallback(({ source }) => {
+  const handleDragEnd = (result) => {
+    if (!result.destination) {
+      return;
+    }
+    const { droppableId: startId, index: startRow } = result.source;
+    const { droppableId: endId, index: endRow } = result.destination;
+    const [startCol, endCol] = [Number(startId.at(-1)), Number(endId.at(-1))];
+    const newItems = reorder(itemLists, [startCol, startRow], [endCol, endRow]);
+    setItemLists(newItems);
+    setSrcDraggable(null);
+    setDstDraggable(null);
+  };
+
+  const handleDragStart = ({ source, draggableId }) => {
     const droppableIdx = Number(source.droppableId.at(-1));
-    setSrcDroppableIdx(droppableIdx);
-  }, []);
+    setSrcDraggable({
+      row: source.index,
+      col: droppableIdx,
+      id: draggableId,
+    });
+    setDstDraggable(null);
+  };
+
+  const handleDragUpdate = ({ destination, draggableId }) => {
+    console.log(destination);
+    if (destination === null) {
+      // setDstDraggable(null);
+      return;
+    }
+
+    const droppableIdx = Number(destination.droppableId.at(-1));
+    setDstDraggable({
+      row: destination.index,
+      col: droppableIdx,
+      id: draggableId,
+    });
+  };
 
   return (
-    <DragDropContext onDragStart={onDragStart} onDragEnd={onDragEnd}>
+    <DragDropContext
+      onDragStart={handleDragStart}
+      onDragEnd={handleDragEnd}
+      onDragUpdate={handleDragUpdate}
+    >
       <section className={style.flexContainer}>
         {itemLists.map((itemList, index) => (
           <DroppableWrapper
             key={`droppable-${index}`}
             itemList={itemList}
             droppableIdx={index}
-            srcDroppableIdx={srcDroppableIdx}
+            itemLists={itemLists}
+            srcDraggable={srcDraggable}
+            dstDraggable={dstDraggable}
+            // setDstDraggable={setDstDraggable}
           />
         ))}
       </section>
