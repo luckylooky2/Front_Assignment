c82533d44cfaca4941240ea794fa52a702adba29
luckylooky2 <luckylooky2@naver.com>
2024-08-23 11:39:17 +0900

    Design: Droppable 간 gap 삭제 및 border로 대체

--- src/styles.js
+++ src/styles.js
@@ -3,16 +3,18 @@ import { css } from '@emotion/css';
 import { GRID } from './constant';
 
 export const getDndStyles = {
-  item: (isDragging, draggableStyle) =>
+  item: (isDragging, draggableStyle, canDrop) =>
     css({
       userSelect: 'none',
       padding: `${GRID * 2}px`,
       margin: `0 0 ${GRID}px 0`,
-      background: isDragging ? 'lightgreen' : 'grey',
+      background: isDragging ? (canDrop ? 'lightgreen' : 'red') : 'grey',
+      transition: 'background-color 0.5s ease-in-out',
       ...draggableStyle,
     }),
   list: (isDraggingOver) =>
     css({
+      border: `${GRID}px solid white`,
       background: isDraggingOver ? 'lightblue' : 'lightgrey',
       padding: `${GRID}px`,
       width: '250px',
@@ -20,5 +22,5 @@ export const getDndStyles = {
 };
 
 export const getLayoutStyles = {
-  flexContainer: css({ display: 'flex', gap: 10 }),
+  flexContainer: css({ display: 'flex' }),
 };
