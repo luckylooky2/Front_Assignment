54c3469165c3a1e34a7c0056d2a00e0414074741
luckylooky2 <luckylooky2@naver.com>
2024-08-24 16:58:54 +0900

    Feat: 다중 선택 onClick 이벤트 처리

--- src/DraggableWrapper.jsx
+++ src/DraggableWrapper.jsx
@@ -1,3 +1,4 @@
+import { cx } from '@emotion/css';
 import PropTypes from 'prop-types';
 import React from 'react';
 import { Draggable } from 'react-beautiful-dnd';
@@ -7,31 +8,63 @@ import { getDndStyles } from './styles';
 const DraggableWrapper = ({
   item,
   itemIndex,
+  droppableIdx,
   srcDraggable,
+  setSrcDraggable,
   dstDraggableState,
+  firstPicked,
 }) => {
   const styles = getDndStyles;
   const { isValid, invalidMsg } = dstDraggableState;
 
+  const handleClick = ({ target }) => {
+    const { rowIdx, colIdx, rbdDraggableId } = target.dataset;
+    const row = Number(rowIdx);
+    const col = Number(colIdx);
+    const id = Number(rbdDraggableId.split('-')[1]);
+    let isSameCol = false;
+
+    if (srcDraggable.size) {
+      const oldSrcDraggable = [...srcDraggable];
+      isSameCol = col === oldSrcDraggable[0][1].col;
+    }
+    const newSrcDraggable = isSameCol ? new Map(srcDraggable) : new Map();
+
+    if (srcDraggable.has(id) && isSameCol) {
+      newSrcDraggable.delete(id);
+    } else {
+      newSrcDraggable.set(id, { row, col, id: rbdDraggableId });
+    }
+
+    setSrcDraggable(newSrcDraggable);
+  };
+
   return (
     <Draggable key={item.id} draggableId={item.id} index={itemIndex}>
-      {(provided, snapshot) => (
-        <div
-          ref={provided.innerRef}
-          {...provided.draggableProps}
-          {...provided.dragHandleProps}
-          className={styles.item(
-            snapshot.isDragging,
-            provided.draggableProps.style,
-            snapshot.draggingOver !== null && isValid,
-          )}
-        >
-          {item.content}
-          {srcDraggable && !isValid && srcDraggable.id === item.id && (
-            <div className={styles.invalidMsg}>{invalidMsg}</div>
-          )}
-        </div>
-      )}
+      {(provided, snapshot) => {
+        const isSrcDraggable = srcDraggable.has(Number(item.id.split('-')[1]));
+        const isSelected = snapshot.isDragging || isSrcDraggable;
+        const isMulti = isSrcDraggable && srcDraggable.size > 1 && firstPicked;
+        const isMsgOpen =
+          !isValid && firstPicked && firstPicked.id === item.id && invalidMsg;
+        return (
+          <div
+            ref={provided.innerRef}
+            {...provided.draggableProps}
+            {...provided.dragHandleProps}
+            data-col-idx={droppableIdx}
+            data-row-idx={itemIndex}
+            onClick={handleClick}
+            className={cx(
+              styles.item(isSelected, isValid, provided.draggableProps.style),
+              styles.itemBlur(isMulti),
+            )}
+          >
+            {item.content}
+            {isMsgOpen && <div className={styles.invalidMsg}>{invalidMsg}</div>}
+          </div>
+        );
+      }}
     </Draggable>
   );
 };
@@ -41,6 +74,9 @@ export default DraggableWrapper;
 DraggableWrapper.propTypes = {
   item: PropTypes.object,
   itemIndex: PropTypes.number,
+  droppableIdx: PropTypes.number,
   srcDraggable: PropTypes.object,
+  setSrcDraggable: PropTypes.func,
   dstDraggableState: PropTypes.object,
+  firstPicked: PropTypes.object,
 };
