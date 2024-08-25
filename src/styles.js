c7a116dd2a08d2d1bf2c357426d57db78cd83e61
luckylooky2 <luckylooky2@naver.com>
2024-08-25 21:13:28 +0900

    Design: droppableContainer 중앙 정렬

--- src/styles.js
+++ src/styles.js
@@ -64,7 +64,7 @@ export const getDndStyles = {
 
 export const getLayoutStyles = {
   appContainer: css({ display: 'relative' }),
-  droppableContainer: css({ display: 'flex' }),
+  droppableContainer: css({ display: 'flex', justifyContent: 'center' }),
 };
 
 export const getNotificationStyles = {
