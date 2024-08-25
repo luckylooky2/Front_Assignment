27485ea5d57914b3a50d11380dd423f5053dfabb
luckylooky2 <luckylooky2@naver.com>
2024-08-25 16:14:01 +0900

    Feat: 초기 srcDraggable 상태 생성 함수

--- src/constant.js
+++ src/constant.js
@@ -1,4 +1,7 @@
+import { initialSrcDraggableGenerator } from './utils';
+
 export const COLUMN_COUNT = 4;
 export const INITIAL_ITEM_COUNT = 10;
 export const BANNED_COLUMN_MOVING_RULES = [[0, 2]];
 export const GRID = 8;
+export const INITIAL_SRC_DRAGGABLE = initialSrcDraggableGenerator([1, 2, 3]);
