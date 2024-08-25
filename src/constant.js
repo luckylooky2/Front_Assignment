a3aca87c08293614e721f4ef7783959989c31696
luckylooky2 <luckylooky2@naver.com>
2024-08-25 21:26:42 +0900

    Fix: 초기 선택값 해제

--- src/constant.js
+++ src/constant.js
@@ -4,4 +4,4 @@ export const COLUMN_COUNT = 4;
 export const INITIAL_ITEM_COUNT = 10;
 export const BANNED_COLUMN_MOVING_RULES = [[0, 2]];
 export const GRID = 8;
-export const INITIAL_SRC_DRAGGABLE = initialSrcDraggableCreator([1, 2, 3]);
+export const INITIAL_SRC_DRAGGABLE = initialSrcDraggableCreator([]);
